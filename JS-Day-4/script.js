d4b33e932071108f9f724a045634fd264a391c56
Mohit Tanwar <mohittanwar@Mohits-MacBook-Air.local>
2026-01-22 11:46:38 +0530

    JS-Day-3

---
+++ JS-Day-4/script.js
@@ -0,0 +1,22 @@
+// Create an object to store user information
+// Object stores data in key-value pairs. It is like dictionary in python
+
+let user={
+    Fullname: "Mohit Tanwar",
+    Age: 18,
+    Email: "abc@gmail.com",
+    Address: "India",
+    Phone: "1234567890",
+    Favcolour: ["Red", "Blue", "Green"]
+}
+console.log(user);
+console.log(user.Fullname , user.Favcolour[1]); //dot notation
+console.log(user["Email"]); //bracket notation
+
+
+let date = new Date();
+console.log(date);
+
+
+
+
