505dec64d8082d5352655dc354f477230c561743
Mohit Tanwar <mohittanwar@Mohits-MacBook-Air.local>
2026-02-03 12:32:05 +0530

    Add Task-1 files

--- Task-1/script.js
+++ Task-1/script.js
@@ -1,19 +1,60 @@
 // Task-1
 
-const profile = document.getElementById("profile");
+// const profile = document.getElementById("profile");
 
-profile.style.backgroundColor = "lightyellow"
-profile.style.padding = "15px"
-profile.style.textAlign = "center"
+// profile.style.backgroundColor = "lightyellow"
+// profile.style.padding = "15px"
+// profile.style.textAlign = "center"
 
 
 // Task-2
 
-const highlight = document.getElementsByClassName("important");
-console.log(highlight.length)
+// const highlight = document.getElementsByClassName("important");
+// console.log(highlight.length)
 
-for(let i=0;i<highlight.length;i++){
+// for(let i=0;i<highlight.length;i++){
 
-    highlight[i].style.color = "red"
-    highlight[i].style.fontSize = "40px"
+//     highlight[i].style.color = "red"
+//     highlight[i].style.fontSize = "40px"
+// }
+
+
+// Task-3
+
+// const task3 = document.getElementsByTagName("p");
+// console.log(task3)
+
+// for(let i=0;i<task3.length;i++){
+//     if(i%2===0 ){
+//         task3[i].style.color = "blue"
+//     }
+//     else{
+//         task3[i].style.color = "green"
+//     }
+// }
+
+// task3[task3.length - 1].style.fontWeight = "bold";
+
+
+
+// Task-4
+
+// const task4 = document.querySelector(".box");
+// console.log(task4);
+
+// for(let i=0;i<task4.length;i++){
+//     task4[i].style.backgroundColor="lightblue";
+// }
+
+// task4.textContent = "I am box 1!";
+
+
+
+// Task-5
+
+const section = document.getElementById("content");
+const task5 = section.querySelectorAll("p");
+
+for(let i=0;i<task5.length;i++){
+    task5[i].style.color = "purple";
 }
