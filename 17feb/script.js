1709482d17cf88b245633416ab0c02ce25643e7e
Mohit Tanwar <mohittanwar@Mohits-MacBook-Air.local>
2026-02-17 12:31:48 +0530

    17feb files added

---
+++ 17feb/script.js
@@ -0,0 +1,36 @@
+// console.log('Starting....');
+// function doWork() {
+//     setTimeout(() => {
+//         console.log('Working....');
+//     }, 2000);
+// }
+// doWork();
+// console.log('Finished');
+
+
+
+// console.log('Starting....');
+// function doWork() {
+//     setTimeout(() => {
+//         return 'Working....';
+//     }, 2000);
+// }
+// console.log(doWork());
+// console.log('Finished');
+
+
+
+// console.log('Starting....');
+// function doWork(callback) {
+//     setTimeout(() => {
+//         callback('Working....');
+//     }, 2000);
+// }
+// doWork((data)=>{
+//     console.log(data);
+// });
+// console.log('Finished');
+
+
+
+
