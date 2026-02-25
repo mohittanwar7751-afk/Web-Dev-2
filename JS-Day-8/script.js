dbd8493efb168e992dff1587c51684e380ba82cd
Mohit Tanwar <mohittanwar@Mohits-MacBook-Air.local>
2026-02-25 14:30:31 +0530

    Day-8 fills added

---
+++ JS-Day-8/script.js
@@ -0,0 +1,7 @@
+function handleClick() {
+    document.getElementById("btn").textContent = "Button Clicked!";
+}
+
+function changeColor() {
+    document.getElementById("heading").style.color = "red";
+}
